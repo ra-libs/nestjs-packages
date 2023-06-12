9ad0f65b2413b2bd33287f2e14ff9e6e07a21523
Vinnys Lins <vinnys.lins@willbank.com.br>
2023-06-12 17:33:08 -0300

    feat: add customer id attribute for growthbook (#4)
    * chore: remove .gitkeep
    
    * feat: add customer id attribute for growthbook

--- libs/growthbook/src/lib/growthbook.service.ts
+++ libs/growthbook/src/lib/growthbook.service.ts
@@ -1,12 +1,19 @@
 import { GrowthBook } from '@growthbook/growthbook';
 import { Inject, Injectable } from '@nestjs/common';
+import { GrowthbookOptions } from './growthbook.interface';
 
 @Injectable()
 export class GrowthbookService {
   @Inject('GROWTHBOOK_CLIENT')
   private client!: GrowthBook;
 
-  isOn(name: string) {
+  isOn(name: string, options: GrowthbookOptions = {}) {
+    if (options.customerId) {
+      this.client.setAttributeOverrides({
+        customerId: options.customerId,
+      });
+    }
+
     return this.client.isOn(name);
   }
 }
