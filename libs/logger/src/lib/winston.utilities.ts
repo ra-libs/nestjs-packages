2f6824008fd30c962aac3803fe7b09ef36b37527
Rabah Zeineddine <rabah.zeineddine@willbank.com.br>
2023-07-09 15:48:04 -0300

    feat(logger): add nestjs-like format for local development (#29)

--- libs/logger/src/lib/winston.utilities.ts
+++ libs/logger/src/lib/winston.utilities.ts
@@ -1,8 +1,19 @@
 import { format } from 'winston';
 import { Format, TransformableInfo } from 'logform';
 import { LogLevel } from './@types';
+import safeStringify from 'fast-safe-stringify';
+import { inspect } from 'util';
 
 export const getFormat = (): Format => {
+  if (process.env['NODE_ENV'] === 'development') {
+    return format.combine(
+      format.errors({ stack: true }),
+      format.timestamp(),
+      format.ms(),
+      nestLikeConsoleFormat()
+    );
+  }
+
   return format.combine(
     format.errors({ stack: true }),
     format.timestamp(),
@@ -46,3 +57,74 @@ export const getLogLevel = (): string => {
     process.env['NODE_ENV'] === 'production' ? LogLevel.INFO : LogLevel.DEBUG;
   return process.env['LOG_LEVEL'] || defaultLogLevel;
 };
+
+// For Local Development
+const clc = {
+  bold: (text: string) => `\x1B[1m${text}\x1B[0m`,
+  green: (text: string) => `\x1B[32m${text}\x1B[39m`,
+  yellow: (text: string) => `\x1B[33m${text}\x1B[39m`,
+  red: (text: string) => `\x1B[31m${text}\x1B[39m`,
+  magentaBright: (text: string) => `\x1B[95m${text}\x1B[39m`,
+  cyanBright: (text: string) => `\x1B[96m${text}\x1B[39m`,
+};
+
+const nestLikeColorScheme: Record<string, (text: string) => string> = {
+  info: clc.green,
+  error: clc.red,
+  warn: clc.yellow,
+  debug: clc.magentaBright,
+  verbose: clc.cyanBright,
+};
+
+const nestLikeConsoleFormat = (appName = 'NestWinston'): Format =>
+  format.printf(
+    ({
+      level,
+      message,
+      context,
+      timestamp,
+      fields,
+      ms,
+      ...meta
+    }: TransformableInfo) => {
+      if ('undefined' !== typeof timestamp) {
+        // Only format the timestamp to a locale representation if it's ISO 8601 format. Any format
+        // that is not a valid date string will throw, just ignore it (it will be printed as-is).
+        try {
+          if (timestamp === new Date(timestamp).toISOString()) {
+            timestamp = new Date(timestamp).toLocaleString();
+          }
+        } catch (error) {
+          // eslint-disable-next-line no-empty
+        }
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      const color =
+        nestLikeColorScheme[level] || ((text: string): string => text);
+      const yellow = clc.yellow;
+
+      delete meta['dd'];
+      const stringifiedMeta = safeStringify(meta);
+      const formattedMeta = inspect(JSON.parse(stringifiedMeta), {
+        colors: true,
+        depth: null,
+      });
+
+      if (fields && typeof fields === 'string' && !context) {
+        context = fields;
+      }
+
+      return (
+        `${color(`[${appName}] - `)} ` +
+        ('undefined' !== typeof timestamp ? `${timestamp} ` : '') +
+        `${color(level.toUpperCase())}\t` +
+        ('undefined' !== typeof context
+          ? `${yellow('[' + context + ']')} `
+          : '') +
+        `${color(message)}` +
+        `${formattedMeta !== '{}' ? ` - ${formattedMeta}` : ''}` +
+        ('undefined' !== typeof ms ? ` ${yellow(ms)}` : '')
+      );
+    }
+  );
